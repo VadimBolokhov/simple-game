d6f28823888b8bd24415010c70461cd816d8dafd
VadimBolokhov <wad4@inbox.ru>
2019-09-08 21:14:18 +0300

    dev: Spring boot

---
+++ src/main/java/ru/vbolokhov/simplegame/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.vbolokhov.simplegame;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SimpleGameApplication.class);
+	}
+
+}
